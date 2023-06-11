e5007ffc10a1789ca26c515f849ab0c175535eaf
James Lu <ellipse120@gmail.com>
2023-06-11 17:16:00 +0800

    build: init project

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-white dark:bg-gray-900">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  color-scheme: normal;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <MainHeader />
+    <slot />
+  </div>
+</template>

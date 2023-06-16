36e60a076f6d0dc7330e072fb3a812e62af51d7b
朱禹帆 <823415612@qq.com>
2023-06-16 14:42:29 +0800

    feat(注册): 注册页面调整

--- components/PageHeaderNew.vue
+++ components/PageHeaderNew.vue
@@ -14,32 +14,22 @@ const jumpTo = function (name) {
 }
 
 const menuRef = ref(null);
+const isIntersecting = ref(false)
+const onIntersect = function (isIntersect, entries, observer) {
+  isIntersecting.value = isIntersect
+}
 </script>
 
 <template>
   <header class="h-35">
-    <input
-      type="checkbox"
-      name="hbr"
-      id="hbr"
-      class="hbr peer"
-      hidden
-      aria-hidden="true"
-    />
-    <nav
-      class="w-full bg-white text-black dark:bg-gray-700/80 backdrop-blur navbar shadow-2xl shadow-gray-600/5 border-b border-gray-100 dark:border-gray-800 peer-checked:navbar-active dark:shadow-none"
-    >
+    <input type="checkbox" name="hbr" id="hbr" class="hbr peer" hidden aria-hidden="true" />
+    <nav v-intersect="onIntersect"
+      class="w-full bg-white text-black dark:bg-gray-700/80 backdrop-blur navbar shadow-2xl shadow-gray-600/5 border-b border-gray-100 dark:border-gray-800 peer-checked:navbar-active dark:shadow-none">
       <div class="w-full">
         <div class="md:pt-3 w-full md:gap-0 lg:pt-5">
-          <div
-            class="w-full items-center flex justify-center lg:w-auto divide-gray divide-x"
-          >
-            <a
-              class="relative z-10 flex items-center justify-center text-3xl pr-3"
-              href="/"
-              aria-label="logo"
-              @click="toggle(false)"
-            >
+          <div class="w-full items-center flex justify-center lg:w-auto divide-gray divide-x">
+            <a class="relative z-10 flex items-center justify-center text-3xl pr-3" href="/" aria-label="logo"
+              @click="toggle(false)">
               <img :src="logo" class="h-10 w-12" />
               {{ corporation }}
             </a>
@@ -49,21 +39,13 @@ const menuRef = ref(null);
               <Button label="注册" class="h-8" @click="jumpTo('register')" />
             </div>
           </div>
-          <div
-            ref="menuRef"
-            class="navmenu w-full"
-          >
-            <div class="text-black flex items-center justify-center w-full bg-gray-100 dark:text-gray-300 dark:bg-gray-8 mt-5 lg:pr-4">
-              <div
-                class="font-medium text-base lg:text-sm lg:flex lg:space-y-0"
-              >
-                <NuxtLink
-                  v-for="(route, index) in menus"
-                  :key="route.label + index"
-                  :to="route.to"
+          <div ref="menuRef" class="navmenu w-full">
+            <div
+              class="text-black flex items-center justify-center w-full bg-gray-100 dark:text-gray-300 dark:bg-gray-8 mt-5 lg:pr-4">
+              <div class="font-medium text-base lg:text-sm lg:flex lg:space-y-0">
+                <NuxtLink v-for="(route, index) in menus" :key="route.label + index" :to="route.to"
                   class="block text-black text-xl font-black px-2 h-15 flex items-center justify-center w-30 lg:px-1 transition duration-400 hover:text-indigo-500 dark:hover:text-primaryLight"
-                  @click="toggle()"
-                >
+                  @click="toggle()">
                   <span>{{ route.label }}</span>
                 </NuxtLink>
               </div>
@@ -78,16 +60,18 @@ const menuRef = ref(null);
 <style>
 body {
   font-family: 微软雅黑, Urbanist, sans-serif;
-  @apply bg-white dark:bg-gray-900;
+  @apply bg-white dark: bg-gray-900;
 }
 
 .navbar-active .hamburger div:first-child {
   @apply rotate-45 translate-y-1.5;
 }
+
 .navbar-active .hamburger div:last-child {
   @apply -rotate-45 -translate-y-1;
 }
+
 .navbar-active div:first-child div:first-child div:last-child {
-  @apply block lg:flex;
+  @apply block lg: flex;
 }
 </style>
